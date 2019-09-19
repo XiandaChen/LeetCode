f39fc1dfb87a8e763a6df61530b8f52ecebe857a
XiandaChen <xuc23@psu.edu>
2019-09-18 23:33:08 -0400

    merge k sorted lists

--- CPP/1-50/GenerateParentheses_recursion.cpp
+++ CPP/1-50/GenerateParentheses_recursion.cpp
@@ -8,7 +8,7 @@ public:
     // left/right: remaining num of left/right bracket
     void generateParenthesisDFS(int left, int right, string out, vector<string> &res) {
         if (left > right) return;
-        if (left ==0 && right == 0)
+        if (left == 0 && right == 0)
             res.push_back(out);
         else {
             if (left > 0) generateParenthesisDFS(left-1, right, out+'(', res);
---
+++ CPP/1-50/MergeKSortedLists.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // Using lambda to compare elements
+        auto cmp = [](ListNode* & a, ListNode* & b) {
+            return a->val > b->val;
+        };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> q(cmp);
+        // put first node of each list into priority queue
+        for (auto node : lists) {
+            // must check if node exists
+            if (node) q.push(node);
+        }
+            
+        
+        ListNode *dummyHead = new ListNode(-1), *curr = dummyHead;
+        while (!q.empty()) {
+            // add the minimum node
+            curr->next = q.top();
+            q.pop();
+            curr = curr->next;
+            // put the next node of the list from which minimum node was poped out the queue
+            if (curr->next) q.push(curr->next);
+        }
+        return dummyHead->next;
+    }
+};
