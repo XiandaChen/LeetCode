883b80b6ec86663267db1b50b68cb3eb32fe639d
XiandaChen <xuc23@psu.edu>
2019-10-11 18:25:58 -0400

    find all disappared element in arrar

---
+++ CPP/401-500/Q448_FindAllDisapparedElementInArray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+                --i; // keep stay
+            }
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != i + 1)
+                res.push_back(i + 1);
+        }
+        return res;
+    }
+};
