445919ac3244e6131ed844a34d7b72ecf5ce566f
XiandaChen <xuc23@psu.edu>
2019-10-12 22:13:21 -0400

    search 2D matrix

--- CPP/201-300/Q240_Search2DMatrix.cpp
+++ CPP/201-300/Q240_Search2DMatrix.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if (matrix.empty() || matrix[0].empty()) return false; // must check  matrix[0].empty()
-        if (matrix[0][0] > target || matrix.back().back() < target) return false;
         int m = matrix.size(), n = matrix[0].size(), x = m - 1, y = 0;
         while (true) { // binary search from the left-bottom coner
             if (matrix[x][y] == target) return true;
