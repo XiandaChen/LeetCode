c6a322293e58f63aa015fd589a7e2c146ad9335b
XiandaChen <xuc23@psu.edu>
2019-12-06 18:10:45 -0500

    continuous subarray sum

---
+++ CPP/501-600/Q523_ContinuousSubarraySum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        for (int i = 0; i < nums.size(); ++i) {
+            int sum = nums[i];
+            // not need, since subarrar of size at least 2
+            // if (k != 0 && sum % k == 0) return true; 
+            for (int j = i + 1; j < nums.size(); ++j) {
+                sum += nums[j];
+                if (sum == k) return true;
+                if (k != 0 && sum % k == 0) return true;
+            }
+        }
+        return false;
+    }
+};
