425802cbcadc7d0138106f8dfd6ade8bca5bde6a
XiandaChen <xuc23@psu.edu>
2020-01-10 14:56:47 -0500

    h index

---
+++ CPP/201-300/Q274_H-Index_Hash.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>()); // decreasing order
+        for (int i = 0; i < citations.size(); ++i)
+            if (citations[i] <= i) return i;
+        return citations.size();
+    }
+};
