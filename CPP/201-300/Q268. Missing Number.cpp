426619ec027223707944bc048eb3947352bbc762
XiandaChen <xuc23@psu.edu>
2020-10-06 23:15:34 -0400

    XOR

---
+++ CPP/201-300/Q268. Missing Number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = 0;
+        for (int i = 0; i < (int)nums.size(); ++i)
+            res ^= nums[i] ^ (i + 1);
+        return res;
+    }
+};
